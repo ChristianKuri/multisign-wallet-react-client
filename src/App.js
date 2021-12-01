2234b6dab9544d90e8db1621c1b8799e136345aa
Christian Kuri <christian.kuri.martinez@gmail.com>
2021-11-30 21:48:06 -0600

    Create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            Multisign Dapp
+            <span>yeah</span>
+        </div>
+    );
+}
+
+export default App;
